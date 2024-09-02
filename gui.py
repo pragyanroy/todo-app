4dd775d241b4715f728cf831adc8a7db02a5c60d
Pragyan Roy <roy.pragyan@gmail.com>
2024-09-02 17:32:08 -0400

    v2

--- gui.py
+++ gui.py
@@ -2,9 +2,21 @@ import functions_todo
 import FreeSimpleGUI as sg
 #1
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a todo")
+input_box = sg.InputText(tooltip="Enter a todo",key = "todo")
 add_button = sg.Button("Add")
 window= sg.Window("My To-Do Application",layout = [[label, input_box,add_button]])
 
-window.read()
+while True:
+    event,values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions_todo.get_todos()
+            new_todo = values['todo']
+            todos.append(new_todo)
+            functions_todo.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
 window.close()
